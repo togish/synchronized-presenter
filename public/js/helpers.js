761cd8139b173f8998f777457971f12979474910
Tor Hvalsøe <tor@itdude.dk>
2014-03-20 22:22:21 +0100

    Network request builder in helper

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,3 +1,17 @@
+// Ensureing browser compatibility for older microsoft browsers.
+if (typeof XMLHttpRequest === "undefined") {
+	XMLHttpRequest = function () {
+		try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+		catch (e) {} 
+		try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+		catch (e) {}
+		try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+		catch (e) {}
+		throw new Error("This browser does not support XMLHttpRequest.");
+	};
+}
+
+// Parsing all GET data parmeters
 var UrlParams = function () {
 	var query_string = {};
 	var query = window.location.search.substring(1);
