d0f641d76a4dbf4c02c3131bc4073dc1c077c43e
Tor Hvalsøe <tor@itdude.dk>
2014-03-25 11:10:43 +0100

    Edit value of segues using inc-decremential buttons

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -129,11 +129,134 @@ var Timeline = function (presentation, timelineBlock, editSegueCallback) {
 					'<polygon class="fill" points="0,22 1,22 1,25 0,25"></polygon></svg>';
 				} else {
 					segueElement.style.background = sgSource.color;
+
+					// Tries to ajust the value of a segue. Returns true if success!
+					var valueAjust = function(input){
+						// Function for encoding seconds into human representation
+						var multiplier = [1, 60, 3600, 86400];
+						var vTextBuilder = function(cont, multi, rest){
+							if (multi < 0) {
+								return cont;
+							} else if(rest == 0 && cont.length == 0){
+								return "0:00";
+							}
+							// Rest to next dimention
+							var nRest = rest % multiplier[multi];
+							var nNum = ((rest - nRest) / multiplier[multi]);
+							cont += cont.length == 0 && multi == 0 ? "0" : "";
+							if(nNum > 0 || cont.length > 0){
+								cont += cont.length > 0 ? ":" : "";
+								cont += ("" + nNum).length < 2 && cont.length > 0 ? "0"+nNum : ""+nNum;
+							}
+							// Continues the process
+							return vTextBuilder(cont, multi-1, nRest);
+						};
+
+						var seconds = segue.value;
+						if(typeof input == "string"){
+							var newValue = null;
+							// I know fucking regular expressions! bitches!
+							newValue = sgSource.timed ? input.match(/[0-9]+\:[0-5][0-9]/) : input.match(/[1-9][0-9]?|^[0]$/);
+							if(!(newValue == null || newValue.length > 1)){
+								// Set the value in the segue. Convert to seconds or slidenumber. Works either case.
+								var seconds = newValue[0].split(":").reduceRight(function(cont, val, idx, arr){
+									return cont + multiplier[arr.length - idx -1] * val;
+								}, 0);
+							}
+						} else if(typeof input == "number"){
+							seconds = input;
+						}
+
+						var ret = false;
+						// Checks that the value is not out of bounds
+						if(0 <= seconds && seconds < sgSource.length){
+							segue.value = seconds;
+							ret = true;
+						}
+
+						// Build the string representation of the value
+						segueValue.value = sgSource.timed ? vTextBuilder("", multiplier.length-1, segue.value) : segue.value;
+
+						// Ajust the size of the input field
+						var inputLen = segueValue.value.length;
+						// console.debug("" + inputLen + " = " + (inputLen > 0 ? Math.round(inputLen / 2) : 2));
+						segueValue.size = inputLen > 0 ? Math.round(inputLen / 2) : 2;
+
+						// Update the length of the segue on timed sources
+
+						// TODO What is this shit!
+						// if(sgSource.timed){
+						// 	var length = sgSource.length - segue.value;
+						// 	segueElement.style.width = ''+length*5+'px';
+						// }
+						return ret;
+					};
+
+					var valueAjustRelative = function(rel){
+						valueAjust(segue.value + rel);
+					};
+
+
+					// Builds the bar for ajusting the offset of the segue
+					var offsetAjust = document.createElement('a');
+					offsetAjust.className = "offset-ajust";
+					offsetAjust.innerHTML = "|";
+					// TODO Add drag and drop on the segue ajuster
+
+					// Building html element for the value ajust substract
+					var valueSub = document.createElement('a');
+					valueSub.className = "focused-visible value-sub";
+					valueSub.innerHTML = "-";
+					valueSub.addEventListener('click', function(e){
+						e.preventDefault();
+						valueAjustRelative(-1);
+					});
+
+					// Building html element for the value ajust add
+					var valueAdd = document.createElement('a');
+					valueAdd.className = "focused-visible value-add";
+					valueAdd.innerHTML = "+";
+					valueAdd.addEventListener('click', function(e){
+						e.preventDefault();
+						valueAjustRelative(1);
+					});
+
+					// Building html element for the value enter field
 					var segueValue = document.createElement('input');
 					segueValue.type="text";
-					// TODO Format correctly!
-					segueValue.value = segue.value;
-					
+					valueAjust();
+
+					// Handling the state of the segues focus and show settings
+					var focused = false;
+					var focusedInput = false;
+					var mouseOver = false;
+					var focusClass = function(show){
+						focused = show;
+						if(show){
+							segueElement.classList.add("focused");
+							_timelineContainer.classList.add("focused");
+						} else {
+							segueElement.classList.remove("focused");
+							_timelineContainer.classList.remove("focused");
+						}
+					}
+				
+					// Marks the segue as focused as the working element
+					segueValue.addEventListener("focus", function(e){
+						focusClass(true);
+						focusedInput = true;
+					});
+
+					// Attach on focus leave listner for the 
+					segueValue.addEventListener("blur", function(e){
+						// Tries to execute a value update
+						focusedInput = false;
+						valueAjust(segueValue.value);
+						if(!mouseOver){
+							focusClass(false);
+						}
+					}, false);
+
 					// Handles enter presses for ending the edit
 					segueValue.addEventListener("keyup", function(e){
 						if(e.keyCode == 13){
@@ -141,30 +264,33 @@ var Timeline = function (presentation, timelineBlock, editSegueCallback) {
 							segueValue.blur();
 						}
 					});
-					// Attach on focus leave listner for the 
-					segueValue.addEventListener("blur", function(e){
-						// I know fucking regular expressions! bitches!
-						var newValue = sgSource.timed ? e.target.value.match(/[0-9]+\:[0-5][0-9]/) : e.target.value.match(/[1-9][0-9]+|^[0]$/);
-						console.debug(newValue);
-						if(newValue == null || newValue.length > 1){
-							e.target.value = segue.value;
-							// TODO Add aditional notification of the mishap
-							return;
+
+					// Shows the segue when clicked
+					segueElement.addEventListener("click", function(e){
+						segueValue.focus();
+					});
+
+					segueElement.addEventListener("mouseover", function(e){
+						mouseOver = true;
+					});
+
+					segueElement.addEventListener("mouseout", function(e){
+						mouseOver = false;
+
+						// Hide if needed
+						if(!focusedInput){
+							focusClass(false);
 						}
-						// Set the value in the segue
-						segue.value = newValue[0];
-						e.target.value = segue.value;
-						
-						// Update the length of the segue on timed sources
-						if(sgSource.timed){
-							var length = sgSource.length - segue.value;
-							segueElement.style.width = ''+length*5+'px';
-						}
-					}, false);
-					// TODO Add drag and drop for moving a segue in time..?
-					// Or show a tooltip box for setting the offset, when in edit mode.
+					});
+
+
+					// Sets up the html
+					segueElement.appendChild(offsetAjust);
+					segueElement.appendChild(valueSub);
 					segueElement.appendChild(segueValue);
+					segueElement.appendChild(valueAdd);
 				}
+
 				timelineElement.appendChild(segueElement);
 				segue.htmlElement = segueElement;
 			}, undefined);
